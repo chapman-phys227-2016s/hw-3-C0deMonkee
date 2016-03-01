b9cfb07bbbd86de6a16914ecba6dd7062f1121cb
Austin Ayers <ayers111@mail.chapman.edu>
2016-03-01 07:05:00 +0000

    Partially done

---
+++ loan.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+"""
+File: Loan.py
+Copyright (c) 2016 Austin Ayers
+License: MIT
+
+Course: PHYS227
+Assignment: A.4
+Date: Feb 11, 2016
+Email: ayers111@mail.chapman.edu
+Name: Austin Ayers
+Description: Implementing equations A.16 and A.17
+"""
+def equations(L, n, p):
+    """
+    Implementing equations A.16 and A.17
+    """
+    x_prev = L
+    for i in range(n):
+        y = ( p / 1200 * x_prev ) + ( L / float(n) )
+        x_temp = x_prev + ( p / 1200 * x_prev ) - y
+        print y
+        print x_temp
+        x_prev = x_temp
---
+++ root_finder_examples.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+"""
+File: root_finder_examples.py
+Copyright (c) 2016 Austin Ayers
+License: MIT
+
+Course: PHYS227
+Assignment: A.4
+Date: Feb 11, 2016
+Email: ayers111@mail.chapman.edu
+Name: Austin Ayers
+Description: Implements different methods for finding a root
+"""
+import numpy as np
+
+def sin(x):
+    return np.sin(x)
+def cos(x):
+    return np.cos(x)
+def display(f, f_prime, a, b, x_0, x_1):
+    n_array = Newton(f,f_prime, x_0)
+    b_array = Bisection(f, a, b)
+    s_array = Secant(f, x_0, x_1)
+def Newton(f, dfdx, x, epsilon=1.0E-7, N=100):
+    n = 0
+    array = []
+    while(abs(f(x)) > epsilon and n <= N):
+        array.append(x)
+        x = x - f(x)/dfdx(x)
+        n += 1
+    return array
+def Bisection(f, a, b, eps=1.0E-5):
+    fa = f(a)
+    i = 0
+    array = []
+    while b - a > eps:
+        i++
+        m = (a + b) / 2.0
+        fm = f(m)
+        if fa*fm <= 0:
+            b = m
+        else:
+            a = m
+            fa = fm
+        array.append([i,a,b])
+    return array
+def Secant():
+    
